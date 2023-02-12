b59faf966ec281b9903e6aa6a99c9ce616ad5b59
peterantho <peteranthony.arokiya@gmail.com>
2023-02-12 20:38:08 +0530

    new codecommit

--- javaproject1/src/javaprojectpack/gitdemopratise.java
+++ javaproject1/src/javaprojectpack/gitdemopratise.java
@@ -11,8 +11,20 @@ public class gitdemopratise {
 			System.out.println(i);
 		}
 
+	
+	int a=2050;
+	if(a%100==0)
+	
+	{
+		System.out.println("leap year");
 	}
+		else
+		{
+		System.out.println("comman year");
+		}
+	}
+}
 
-	}
+	
 
 
