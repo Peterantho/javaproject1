2e0046efba67ac339858563bb35232e730e29118
peterantho <peteranthony.arokiya@gmail.com>
2023-02-12 20:26:24 +0530

    new commintjavaproject

---
+++ javaproject1/src/javaprojectpack/gitdemopratise.java
@@ -0,0 +1,18 @@
+package javaprojectpack;
+
+public class gitdemopratise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		for(i=1;i<10;i++);
+		{
+			System.out.println(i);
+		}
+
+	}
+
+	}
+
+
